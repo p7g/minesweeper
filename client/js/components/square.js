63daf2ee7315c1a6102ffabd7812bdf5120a218c
ging0044 <775.pg.12@gmail.com>
2019-03-02 18:32:14 -0500

    adjust the square text color depending on number of mines in vicinity

--- client/js/components/square.js
+++ client/js/components/square.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const severity = {
+  1: 'blue',
+  2: 'green',
+  3: 'red',
+  4: 'purple',
+  5: 'maroon',
+  6: 'turquoise',
+  7: 'black',
+  8: 'gray',
+};
+
 /* eslint-disable camelcase, react/prop-types */
 export default ({
   square: {
@@ -11,22 +22,44 @@ export default ({
   disabled,
   onClick,
   onContextMenu,
-}) => (
-  <button
-    disabled={disabled}
-    type="button"
-    className={`
-      nes-btn
-      ${has_mine ? 'is-error' : ''}
-      ${is_revealed && !has_mine ? 'is-primary' : ''}
-      ${has_flag && !is_revealed ? 'is-warning' : ''}
-    `}
-    onClick={onClick}
-    onContextMenu={onContextMenu}
-  >
-    {(is_revealed && ((has_mine && 'M') || `${adjacent_mines}`)) || (
-      has_flag && 'F'
-    ) || 'H'}
-  </button>
-);
+}) => {
+  let contents = '\u00A0';
+  let extraStyles = {};
+
+  if (is_revealed) {
+    if (has_mine) {
+      contents = 'M';
+    } else {
+      extraStyles = {
+        color: severity[adjacent_mines],
+        opacity: '1',
+        cursor: 'inherit',
+      };
+      if (adjacent_mines > 0) {
+        contents = adjacent_mines;
+      }
+    }
+  } else if (has_flag) {
+    contents = 'F';
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      type="button"
+      className={`
+        nes-btn
+        ${has_mine ? 'is-error' : ''}
+        ${is_revealed && !has_mine ? 'is-disabled' : ''}
+        ${has_flag && !is_revealed ? 'is-warning' : ''}
+      `}
+      style={extraStyles}
+      active={is_revealed}
+      onClick={onClick}
+      onContextMenu={onContextMenu}
+    >
+      {contents}
+    </button>
+  );
+};
 /* eslint-enable camelcase, react/prop-types */
