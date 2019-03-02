b927ab6d2cc7a3aa0a9424f9801f9f96ca141460
ging0044 <775.pg.12@gmail.com>
2019-03-02 11:01:47 -0500

    added new patterns for api

--- games/urls.py
+++ games/urls.py
@@ -4,9 +4,12 @@ Routes for the game API
 
 from django.urls import path
 
-from .views import GameIndexView
+from .views import GameIndexView, GameView, SquareFlagView, SquareRevealView
 
 app_name = 'games'
 urlpatterns = [
-    path('/', GameIndexView.as_view()),
+    path('games/<int:game_id>', GameView.as_view()),
+    path('games', GameIndexView.as_view()),
+    path('squares/<int:square_id>/flag', SquareFlagView.as_view()),
+    path('squares/<int:square_id>/reveal', SquareRevealView.as_view()),
 ]
