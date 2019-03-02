5149b5a7a6503df35b61128174d35d6cfb017403
ging0044 <775.pg.12@gmail.com>
2019-03-02 10:49:42 -0500

    added wildcard routes for serving client static files

--- client/views.py
+++ client/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+"""
+View for sending the client application
+"""
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+@ensure_csrf_cookie
+def client(request):
+    """ Always render index.html """
+    return render(request, 'client/index.html')
